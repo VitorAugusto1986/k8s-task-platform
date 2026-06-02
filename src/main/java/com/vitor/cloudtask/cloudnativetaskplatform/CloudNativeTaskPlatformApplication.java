af23391a1fceaefb61ad80fefe93165b859bf789
Vitor Augusto (CTW) <Vitor.Augusto@ctw.bmwgroup.com>
2026-06-02 12:30:24 +0100

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/vitor/cloudtask/cloudnativetaskplatform/CloudNativeTaskPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.cloudtask.cloudnativetaskplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeTaskPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeTaskPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vitor/cloudtask/cloudnativetaskplatform/CloudNativeTaskPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.cloudtask.cloudnativetaskplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeTaskPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
